de150d2522b25f74b1f62d255cc11accb1bcacc4
Ross White <rosswhite@outlook.com>
2021-05-09 17:28:44 +0100

    FIX: Getting Router working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,28 @@
-// const router = require('express').Router();
-// const fs = require('fs');
-// const {v4: uuidv4} = require ('uuid');
-// const db = ('../db/db.json');
+const router = require('express').Router();
+const fs = require('fs');
+const {v4: uuidv4} = require ('uuid');
+const path = require('path');
+const db = path.join(__dirname,'..', 'db', 'db.json');
+
+router.get('/', (req, res) => {
+    res.json(JSON.parse(fs.readFileSync(db, "utf8")));
+});
+
+router.post('/', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync(db, 'utf8'));
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    notes.push(newNote);
+    fs.writeFileSync(db, JSON.stringify(notes, null, 2));
+    res.json(notes);
+});
+
+router.delete("/:id", function(req, res) {
+    let notes = JSON.parse(fs.readFileSync(db, 'utf8'));
+    notes.splice(req.params.id, 1);
+    fs.writeFileSync(db, JSON.stringify(notes, null, 2));
+    res.json(notes);
+});
 
 
-// router.get('/api/notes', (req, res) => {
-//     res.json(JSON.parse(fs.readFileSync(db, "utf8")));
-// });
-
-// router.post('/api/notes', (req, res) => {
-//     let notes = JSON.parse(fs.readFileSync(db, 'utf8'));
-//     const newNote = req.body;
-//     newNote.Id = uuidv4();
-//     notes.push(newNote);
-//     fs.writeFileSync(db, JSON.stringify(notes, null, 2));
-//     res.json(notes);
-// });
-
-// module.exports = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 // Dependencies
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes')
 const htmlRoutes = require('./routes/htmlRoutes');
-// const apiRoutes = require('./routes/apiRoutes');
-
-const fs = require('fs');
-const {v4: uuidv4} = require ('uuid');
-const path = require('path');
-const db = ('./db/db.json');
 
 // Set Server
 const app = express();
@@ -15,32 +10,8 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.static('public'));
-// app.use('/api', apiRoutes);
+app.use('/api/notes', apiRoutes);
 app.use('/', htmlRoutes);
-
-app.get('/api/notes', (req, res) => {
-    res.json(JSON.parse(fs.readFileSync(db, "utf8")));
-});
-
-app.post('/api/notes', (req, res) => {
-    let notes = JSON.parse(fs.readFileSync(db, 'utf8'));
-    const newNote = req.body;
-    newNote.Id = uuidv4();
-    notes.push(newNote);
-    fs.writeFileSync(db, JSON.stringify(notes, null, 2));
-    res.json(notes);
-});
-
-app.get("/api/notes/:id", function(req,res) {
-    res.json(res.json(JSON.parse(fs.readFileSync(db, "utf8")))[req.params.id]);
-});
-
-app.delete("/api/notes/:id", function(req, res) {
-    let notes = JSON.parse(fs.readFileSync(db, 'utf8'));
-    notes.splice(req.params.id, 1);
-    fs.writeFileSync(db, JSON.stringify(notes, null, 2));
-    res.json(notes);
-});
 
 // Listen
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
