99196fd2f2a0328146ed972bae48891b707f7571
Ross White <rosswhite@outlook.com>
2021-05-09 11:50:04 +0100

    Added routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,27 @@
 const router = require('express').Router();
+const { notStrictEqual } = require('assert');
 const fs = require('fs');
-// router.use(express.json());
 
 
 // API Routes
 router.get('/api/notes', function(req, res) {
-    fs.readFile("../db/db.json", "utf8", function(err, data) {
+    fs.readFileSync("../db/db.json", "utf8", function(err, data) {
     if (err) throw err;
     res.json(JSON.parse(data));
     })
 });
+
+router.post('/api/notes', function(req, res) {
+    let newNote = req.body;
+    notStrictEqual.push(newNote);
+    rewriteDB();
+})
+
+const rewriteDB = () => {
+    fs.writeFile('db/db.json', JSON.stringify(notes,), err => {
+        if (err) throw err;
+        return true;
+    })
+}
 
 module.exports = router;
